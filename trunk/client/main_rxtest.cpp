d818f563ce52fd3590e0d3f6c384b1ee2b355a4d
josiahbryan <josiahbryan@49c7cdab-f878-ea45-bc0b-0b624495f101>
2012-03-25 14:22:11 +0000

    Fixed bug - 'Apply' button in client/MidiInputSettingsDialog.cpp wasn't connected - argggg.

--- trunk/client/main_rxtest.cpp
+++ trunk/client/main_rxtest.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
 	QCoreApplication app(argc,argv);
 	
 	MidiReceiver reader;
-	reader.start();
+	reader.start("192.168.0.18");
 	
 	qDebug() << "Started MIDI Receiver.";
 	
