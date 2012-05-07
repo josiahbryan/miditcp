ad4846b34a8fa7d2f3d96ce31c0a145cd9df826e
josiahbryan <josiahbryan@49c7cdab-f878-ea45-bc0b-0b624495f101>
2012-05-07 18:26:44 +0000

    Updated server code to automatically read from first midi* device in /dev/snd

--- trunk/server/main.cpp
+++ trunk/server/main.cpp
@@ -1,14 +1,39 @@
 #include <QCoreApplication>
+#include <QDir>
+#include <QStringList>
 
 #include "MidiReader.h"
 #include "MidiServer.h"
 
+#define MIDI_DIR "/dev/snd"
+
 int main(int argc, char *argv[])
 {
 	QCoreApplication app(argc,argv);
+
+	QDir dir(MIDI_DIR);
+	if(!dir.exists())
+	{
+		qDebug() << "Error: Unable to find '" MIDI_DIR "' - exiting.";
+		exit(-1);
+	}
+
+	QStringList midiList = dir.entryList(QStringList() << "midi*", QDir::System);
+	qDebug() << "Debug: Midi devices found: "<<midiList;
 	
-	MidiReader reader("/dev/snd/midi1");
+	if(midiList.isEmpty())
+	{
+		qDebug() << "Error: No devices found matching 'midi*' in '" MIDI_DIR "' - exiting.";
+		exit(-1);
+	}
+
+	QString midiDevice = QString("%1/%2").arg(MIDI_DIR).arg(midiList.first());
+	qDebug() << "Debug: Using device: "<<midiDevice;
+	
+	//MidiReader reader("/dev/snd/midi3");
 	//MidiReader reader("/dev/snd/midiC0D0");
+	//MidiReader reader("/dev/snd/midiC3D0");
+	MidiReader reader(midiDevice);
 	reader.start();
 	
 	qDebug() << "Started MIDI Reader Thread.";
